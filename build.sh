c5b4f2b6fadda22c200014b3974dae14a0067c1b
Andreas Gies <andreas@wayofquality.de>
2015-08-24 11:37:03 +0200

    Updating build file.

--- build.sh
+++ build.sh
@@ -12,3 +12,21 @@ set -e
 # build site with jekyll, by default to `_site' folder
 jekyll build
 
+# cleanup
+rm -rf ../riodelamiel.github.io.master
+
+#clone `master' branch of the repository using encrypted GH_TOKEN for authentification
+git clone https://${GH_TOKEN}@github.com/riodelamiel/riodelamiel.github.io.git ../riodelamiel.github.io.master
+
+# copy generated HTML site to `master' branch
+cp -R _site/* ../riodelamiel.github.io.master
+
+# commit and push generated content to `master' branch
+# since repository was cloned in write mode with token auth - we can push there
+cd ../riodelamiel.github.io.master
+git config user.email "andreas@wayofquality.de"
+git config user.name "Andreas Gies"
+git add -A .
+git commit -a -m "Travis #$TRAVIS_BUILD_NUMBER"
+git push --quiet origin master > /dev/null 2>&1 
+
