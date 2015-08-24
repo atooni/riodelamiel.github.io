f58cfbebe6a824ad2865c2f2e79e53da66458239
Andreas Gies <andreas@wayofquality.de>
2015-08-24 10:46:49 +0200

    Adding build files.

---
+++ build.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+# only proceed script when started not by pull request (PR)
+if [ $TRAVIS_PULL_REQUEST == "true" ]; then
+  echo "this is PR, exiting"
+  exit 0
+fi
+
+# enable error reporting to the console
+set -e
+
+# build site with jekyll, by default to `_site' folder
+jekyll build
+
