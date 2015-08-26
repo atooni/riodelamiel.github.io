cd3d2756620a4312012d7986e50acf95e4db911a
Andreas Gies <andreas@wayofquality.de>
2015-08-26 16:03:57 +0200

    Adding Map and proper configuration.

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,24 @@
-$(".nav li a").on("click", function() {
-  $(".nav").find(".active").removeClass("active");
-  $(this).parent().addClass("active");
-  return true;
-});
+var gmap = null;
+
+
+$( document ).ready(
+  function() {
+    $("#map").each( function(idx, elem) {
+      gmap = new GMaps({
+        div: "#" + elem.id,
+        lat: 36.800024,
+        lng: -3.776859,
+        zoom: 17,
+        mapType: "satellite"
+      });
+
+      gmap.addMarker({
+        lat: 36.800024,
+        lng: -3.776859,
+        infoWindow: {
+          content: "<p>See you at Rio de la Miel!</p>"
+        }
+      });
+    });
+  }
+);
